f400ab35ac9873e9aa0ca93dcfd165d4b9841d87
malrang <comori4@gmail.com>
2021-04-05 23:13:46 +0900

    업로드 화면 추가중

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,5 +1,26 @@
+import { Form, Divider, Input } from "antd";
+import "./index.css";
+
 function UploadPage() {
-  return <h1>업로드화면</h1>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <div>
+      <Form name="상품 업로드" onFinnish={onSubmit}>
+        <Form.Item name="upload">
+          <div id="upload-img-placeholder">
+            <img src="/images/icons/camera.png" />
+            <span>이미지를 업로드해주세요.</span>
+          </div>
+        </Form.Item>
+        <Divider />
+        <Form.Item>
+          <Input size="large" placeholder="이름을 입력해주세요." />
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 export default UploadPage;
